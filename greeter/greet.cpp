25da40a6c68eb6e24f95caec2e36665562a3a2bb
Felix Benning <felix.benning@gmail.com>
2021-02-26 15:42:37 +0100

    fix pipeline

--- greeter/greet.cpp
+++ greeter/greet.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <sstream>
 
-auto greet(std::string name) {
+std::string greet(std::string name) {
     std::ostringstream stream;
     stream << "Hello, " << name << "!" << std::endl;
     return stream.str();
